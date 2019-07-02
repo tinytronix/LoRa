7a7d559e5ff49354b841ebb1c56a0d8beb58aaf9
tinytronix <49196001+tinytronix@users.noreply.github.com>
2019-07-02 08:15:18 +0200

    Update LoraCommon.h

--- LoraCommon.h
+++ LoraCommon.h
@@ -23,25 +23,25 @@ typedef enum _tagLORAMDID
 	//Requests immer ungerade, Responses immer gerade Zahl
 	//Diese Festlegung wird benutzt, um Gateway-Funktionen und Node-Funktionen 
 	//auseinander zu halten
-	LORA_ACTOR_REQ												=	0x01,
-	LORA_ACTOR_RESP												=	0x02,
-	LORA_SENSOR_REQ												=	0x03,
-	LORA_SENSOR_RESP											=	0x04,
-	LORA_EEPWRITE_REQ											=	0x05,
-	LORA_EEPWRITE_RESP										=	0x06,
-	LORA_EEPREAD_REQ											=	0x07,
-	LORA_EEPREAD_RESP											=	0x08,
-	LORA_PROPERTY_REQ											=	0x09,
-	LORA_PROPERTY_RESP										=	0x0A
+	LORA_ACTOR_REQ				= 0x01,
+	LORA_ACTOR_RESP				= 0x02,
+	LORA_SENSOR_REQ				= 0x03,
+	LORA_SENSOR_RESP			= 0x04,
+	LORA_EEPWRITE_REQ			= 0x05,
+	LORA_EEPWRITE_RESP		= 0x06,
+	LORA_EEPREAD_REQ			= 0x07,
+	LORA_EEPREAD_RESP			= 0x08,
+	LORA_PROPERTY_REQ			= 0x09,
+	LORA_PROPERTY_RESP		= 0x0A
 }LORACMDID;
 
 //GW
 typedef struct _tagLORAHEAD
 {
-  uint32_t  radioId;                                  //id des Empfaengers 
-  uint8_t  	transactionContext;                       //wird vom Gateway fuer jeden Request neu vergeben und muss zurueckgeschickt werden, falls eine Antwort erwartet wird
-  uint8_t   cmdId;                                		//LORACMD, LORAACK, LORABRIDGE etc.
-  uint8_t   status;                                   //ACK etc                                                        
+  uint32_t radioId;                              //id des Empfaengers 
+  uint8_t  transactionContext;                   //wird vom Gateway fuer jeden Request neu vergeben und muss zurueckgeschickt werden, falls eine Antwort erwartet wird
+  uint8_t  cmdId;                                //LORACMD, LORAACK, LORABRIDGE etc.
+  uint8_t  status;                               //ACK etc                                                        
 }LORAHEAD;
 
 
