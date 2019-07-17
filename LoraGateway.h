4dcfabf32d9d7ce6880dc68a4c0c3865d76d2ab4
tinytronix <49196001+tinytronix@users.noreply.github.com>
2019-07-17 15:13:11 +0200

    indents corrected

--- LoraGateway.h
+++ LoraGateway.h
@@ -7,8 +7,8 @@
 
 typedef struct _tagLORATRANSACTION
 {
-  LORAFRAME       			loraFrameTx;
-  LORAFRAME       			loraFrameRx;
+  LORAFRAME       	loraFrameTx;
+  LORAFRAME       	loraFrameRx;
   uint16_t              timeout;
   uint16_t              state;
   uint8_t               nRepeat;
@@ -24,32 +24,32 @@ class LoraGateway : private SX126x
     LoraGateway(int spiSelectPin, int resetPin, int busyPin, int interruptPin);
     ~LoraGateway();
     
-    void begin                      (uint8_t taskIntervalInMs, uint8_t spreadingFactor, uint32_t frequencyInHz, int8_t txPowerInDbm);  
-    void LoRaConfig                 (uint8_t spreadingFactor, uint8_t bandwidth, uint8_t codingRate, uint16_t preambleLength, uint8_t payloadLen, bool crcOn, bool invertIrq);
-		void Service                    (void);
-    void Send                       (uint32_t radioId, uint8_t cmdId, uint8_t *data, uint8_t len);
-    uint8_t GetResponseData         (uint8_t **pRespData);
+    void begin                  (uint8_t taskIntervalInMs, uint8_t spreadingFactor, uint32_t frequencyInHz, int8_t txPowerInDbm);  
+    void LoRaConfig             (uint8_t spreadingFactor, uint8_t bandwidth, uint8_t codingRate, uint16_t preambleLength, uint8_t payloadLen, bool crcOn, bool invertIrq);
+    void Service                (void);
+    void Send                   (uint32_t radioId, uint8_t cmdId, uint8_t *data, uint8_t len);
+    uint8_t GetResponseData     (uint8_t **pRespData);
 #ifndef LORANODE_NO_ENCRYPT
-		void EncryptDecryptKey			    (CIPHERKEY * key);
+    void EncryptDecryptKey	(CIPHERKEY * key);
 #endif
 	
- 	private:
- 		LORATRANSACTION									currTransaction;
-    uint8_t                         transactionCtxFactory;
+  private:
+    LORATRANSACTION		currTransaction;
+    uint8_t                     transactionCtxFactory;
 
- 		uint16_t												timer;
- 		uint8_t													taskInterval;						//intervall in dem die Funktion Service aufgerufen wird, wichtig zur Berechnugn der Timer
+    uint16_t			timer;
+    uint8_t		    	taskInterval;  //intervall in dem die Funktion Service aufgerufen wird, wichtig zur Berechnugn der Timer
 #ifndef LORANODE_NO_ENCRYPT
-		CIPHERKEY												encryptDecryptKey;
-		uint8_t													encryptDecryptState;		//0: no encryption, 1: setup required, 2: encrypt prepared
-		spritz_ctx											encryptDecryptCtx;
+    CIPHERKEY			encryptDecryptKey;
+    uint8_t			encryptDecryptState;		//0: no encryption, 1: setup required, 2: encrypt prepared
+    spritz_ctx			encryptDecryptCtx;
 #endif
 
-		void ReceiveResp							  (void);		
-		void Timer										  (void);
-		void ProcessTransaction	        (void);
+    void ReceiveResp		(void);		
+    void Timer			(void);
+    void ProcessTransaction	(void);
 #ifndef LORANODE_NO_ENCRYPT
-    void EncryptDecrypt				      (uint8_t* input, uint8_t* output, uint16_t len);
+    void EncryptDecrypt		(uint8_t* input, uint8_t* output, uint16_t len);
 #endif
 };
 #endif //_LORAGATEWAY_H
